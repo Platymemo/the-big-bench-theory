57ca798150e577e0639749fcb4c5d3c553c05ced
Platymemo <edwardhazelton+github@gmail.com>
2022-01-06 01:01:29 -0900

    fix rei compat on servers (fixes #5)

--- src/main/java/com/github/platymemo/bigbenchtheory/compat/rei/MegaCraftingContainerPlugin.java
+++ src/main/java/com/github/platymemo/bigbenchtheory/compat/rei/MegaCraftingContainerPlugin.java
@@ -1,14 +1,15 @@
 package com.github.platymemo.bigbenchtheory.compat.rei;
 
+import com.github.platymemo.bigbenchtheory.BigBenchTheory;
 import com.github.platymemo.bigbenchtheory.screen.handlers.AbstractBigBenchCraftingScreenHandler;
 import me.shedaniel.rei.plugin.containers.CraftingContainerInfoWrapper;
 import me.shedaniel.rei.server.ContainerInfoHandler;
 import net.minecraft.util.Identifier;
 
-public class MegaCraftingContainerPlugin implements Runnable{
+public class MegaCraftingContainerPlugin implements Runnable {
     @Override
     public void run() {
-        ContainerInfoHandler.registerContainerInfo(MegaCraftingPlugin.MEGA_CRAFTING, CraftingContainerInfoWrapper.create(AbstractBigBenchCraftingScreenHandler.class));
+        ContainerInfoHandler.registerContainerInfo(new Identifier(BigBenchTheory.MOD_ID, "plugins/mega_crafting"), CraftingContainerInfoWrapper.create(AbstractBigBenchCraftingScreenHandler.class));
         ContainerInfoHandler.registerContainerInfo(new Identifier("minecraft", "plugins/crafting"), CraftingContainerInfoWrapper.create(AbstractBigBenchCraftingScreenHandler.class));
     }
 }
